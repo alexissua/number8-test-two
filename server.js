4feb1dce97e39b3c76bb1695286aee8145e9c043
Alexis Suarez <alexissuarez@Alexiss-MacBook-Pro.local>
2017-10-23 23:21:11 -0500

    Url install

--- server.js
+++ server.js
@@ -20,3 +20,19 @@ app.use(express.static("public"));
 app.listen(PORT, function(){
   console.log("Express server is up on " + PORT + " port.");
 });
+
+/*var gzippo = require('gzippo');
+var express = require('express');
+// We have to install morgan. Some of the middleware of express you have to install it separate::
+var morgan = require('morgan');
+
+// Create our app::
+var app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+
+app.listen(PORT, function(){
+  console.log("Express server is up on " + PORT + " port.");
+});*/
